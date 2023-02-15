f0d2280054d886fe7a4fb70af266f31b8fbc89b8
Mher <mher.hov95@gmail.com>
2023-02-15 15:14:10 +0400

    arrays

---
+++ src/arrays/Arrays.java
@@ -0,0 +1,132 @@
+package arrays;
+
+public class Arrays {
+        public static void main(String[] args) {
+            int [] arr = new int[]{1,0,6,4,9,0,0};
+//            for (int i = 0;i < arr.length - 1;i++){
+//                for (int j = 1;j < arr.length;j++){
+//                    if (arr[j]>arr[i]){
+//                        arr[i]=arr[j];
+//                    }
+//                }
+//            }System.out.println(arr[arr.length-1]);
+//            for (int i = 0;i < arr.length - 1;i++){
+//                for (int j = 1;j < arr.length;j++){
+//                    if (arr[i]%2==0){
+//
+//                    } else if (arr[j]%2==0) {
+//                        arr[i]=arr[j];
+//                        if (arr[i-1]%2!=0){
+//                            arr[i - 1]=arr[j];
+//                        }
+//                    }
+//                }
+//                }
+//            System.out.println(arr[0]);
+            for (int i = 0;i < arr.length;i++){
+                if (arr[i]==0){
+
+                }
+            }
+        }
+
+//    /**
+//     * this method returns positive values of the array
+//     * @param arr
+//     * @return
+//     */
+//    public static int myMethod(int[] arr) {
+//        for (int i = 0;i<arr.length;i++){
+//            if (arr[i]>0){
+//                System.out.println(arr[i]);
+//            }
+//        }
+//        return 0;
+//    };
+
+//    /**
+//     * this method change the places of the values from last to first
+//     * @param arr
+//     * @return
+//     */
+//    public static short myMethod(short[] arr){
+//        for ( int i = arr.length -1 ;i>=0;i--){
+//            System.out.println(arr[i]);
+//        }
+//        return 0;
+//    };
+
+        /**
+         * this method find the bigest value
+         * @param arr
+         * @return
+         */
+//    public static long myMethod(long[] arr){
+//        long max = 0;
+//        for (int i = 0;i<arr.length;i++){
+//            if (arr[i]>max){
+//                max = arr[i];
+//            }
+//        }
+//        System.out.println(max);
+//        return 0;
+//    };
+        /**
+         * this method finds the min value
+         */
+//    public static float myMethod(float[] arr){
+//        float min=3.402823E+38f;
+//      for (int i = 0;i < arr.length;i++){
+//          if (arr[i]<min){
+//              min=arr[i];
+//          }
+//      }System.out.println(min);
+//
+//        return 0;
+//    };
+        /**
+         * this method pushes the elements from first array to other
+         */
+//    public static int myMethod(int[]n){
+//        int[] m=new int[n.length];
+//        for (int i = 0;i<n.length;i++){
+//            m[i]=n[i];
+//            System.out.println(m[i]);
+//        }
+//        return 0;
+//    };
+
+        /**
+         * this method creates a new arr from 2 other
+         * @return
+         */
+//    public static int myMethod(){
+//        int n = 4;
+//        int [] arr1 = new int[n];
+//        int [] arr2 = new int[n];
+//        int [] arr3 = new int[n];
+//        for (int i = 0;i<n;i++){
+//            arr3[i]=arr1[i]+arr2[i];
+//        }
+//        System.out.println(arr3);
+//        return 0;
+//    }
+
+        /**
+         * this methods counts how many 5 does array have
+         * @param arr
+         * @return
+         */
+//    public static int myMethod(int [] arr){
+//        int count = 0;
+//        int k = 5;
+//        for (int i = 0; i < arr.length;i++){
+//            if (arr[i] == k){
+//                count++;
+//            }
+//        }
+//        System.out.println(count);
+//        return 0;
+//    }
+    }
+
